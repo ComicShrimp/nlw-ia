b7d61cce36ddab82a8cc1da0f7f294fcfdbf7d3d
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2023-09-13 22:05:22 -0300

    feat: create first route

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,14 +1,12 @@
-import { fastify } from 'fastify';
+import { fastify } from 'fastify'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
 
-const app = fastify();
+const app = fastify()
 
-app.get('/', () => {
-  return "Hello World!";
-}
-);
+app.register(getAllPromptsRoute)
 
 app.listen(
   {
     port: 3333,
   }
-).then(() => console.log('Server is running!'));
+).then(() => console.log('Server is running!'))
