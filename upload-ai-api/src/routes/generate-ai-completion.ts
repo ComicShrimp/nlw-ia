d9fa69f6bb1940bf0c47538d1c8d644826ea0687
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2023-09-14 00:02:48 -0300

    feat: add chatgpt prompt

---
+++ upload-ai-api/src/routes/generate-ai-completion.ts
@@ -0,0 +1,47 @@
+import { FastifyInstance } from "fastify"
+import { prisma } from "../lib/prisma"
+import { z } from "zod"
+import { createReadStream } from "node:fs"
+import { openai } from "../lib/openai"
+
+export async function generateAICompletionRoute(app: FastifyInstance) {
+  app.post('/ai/complete', async (request, response) => {
+
+    const bodySchema = z.object({
+      videoId: z.string().uuid(),
+      template: z.string(),
+      temperature: z.number().min(0).max(1).default(0.5),
+    })
+    
+    const { template, videoId, temperature } = bodySchema.parse(request.body)
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      }
+    })
+
+    if (!video.transcript) {
+      return response.status(400).send({
+        error: 'Video transcript was not generated yet',
+      })
+    }
+
+    const promptMessage = template.replace('{trascription}', video.transcript)
+
+    const reply = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo-16k',
+      temperature,
+      messages: [
+        {
+          role: 'user',
+          content: promptMessage,
+        }
+      ]
+    })
+
+    return response
+
+  }
+  )
+}
